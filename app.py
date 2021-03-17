0203e916d178384a0b6bdcd9a70847507b8a5764
DancingLinks <348368689@qq.com>
2021-03-17 19:48:00 +0800

    1

--- app.py
+++ app.py
@@ -449,4 +449,4 @@ if __name__ == '__main__':
     if block and not init_block(block) or not port:
         sys.exit()
     
-    app.run(host='0.0.0.0',port=port,debug=False)
+    app.run(host='0.0.0.0',port=port,debug=True)
